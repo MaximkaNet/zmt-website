6a6ea4793ffac54ae6e976efec1c10da72951d49
Maksym <m.zavada2005@ukr.net>
2023-12-04 09:16:27 +0100

    Add popup

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,23 @@
-let vh = window.innerHeight * 0.01
-document.documentElement.style.setProperty("--vh", `${vh}px`)
+function fixViewHeight() {
+    const root = document.documentElement
+    let vh = window.innerHeight * 0.01
+    root.style.setProperty("--vh", `${vh}px`)
+}
+
+function main() {
+    const openVideo = document.querySelector("[data-open-video]")
+    const closeVideo = document.querySelector("[data-close-video]")
+    const modal = document.querySelector("[data-popup]")
+    openVideo.addEventListener("click", () => {
+        modal.classList.remove("hidden")
+    })
+
+    closeVideo.addEventListener("click", () => {
+        modal.classList.add("hidden")
+    }, true)
+
+    window.addEventListener('resize', fixViewHeight)
+    window.addEventListener("DOMContentLoaded", fixViewHeight)
+}
+
+main()
